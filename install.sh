4ad1608e21a9a0e32ea768bb5425b21451c4bbdb
alexou2 <alexandreValin2@gmail.com>
2023-05-03 10:04:35 -0400

    added commands and confirmation before installing packages

--- install.sh
+++ install.sh
@@ -6,22 +6,32 @@ dependancies=("libgtk-4-1" "xfce4" "lightdm" "git")
 
 # updates repositories
 
-for package in $dependancies
-do
-# checks for libgtk version
-if dpkg -s libgtk-4-1 >/dev/null 2>&1; then
-    echo "libgtk-4-1 is installed"
-else
-    echo "libgtk-4-1 is not installed"
-    echo "will start installing libgtk-4-1"
-    # sudo apt install libgtk-4-1
-fi
-done
+for package in ${dependancies[@]}
+    do
 
+# checks if every package is installed
+    if dpkg -s $package >/dev/null 2>&1
+        then
+        echo -e "$package is installed\n"
+    else
+        echo "$package is not installed"
+        echo -e "\u0007"
+
+# confims installation of new package
+        read -p "Do you want to install $package? [Y/n] " confirm
+        if [ "$confirm" = "y" ]
+            then
+            sudo apt install $package
+        else
+            echo "Installation of $package aborted"
+        fi
+
+    fi
+done
 
 # clones the rust project from github
 if [ -z $(find xf-tweaks)  ]
-then
-# git clone https://github.com/alexou2/xf-tweaks.git
-echo install
+    then
+    echo "cloning code from github..."
+    # git clone https://github.com/alexou2/xf-tweaks.git
 fi
