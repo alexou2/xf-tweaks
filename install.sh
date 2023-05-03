dab2b81dc3cd8f8f60c52040c503218363c41e1b
alexou2 <alexandreValin2@gmail.com>
2023-05-03 10:08:52 -0400

    un-commented line to clone repository from GH

--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@
 dependancies=("libgtk-4-1" "xfce4" "lightdm" "git")
 
 
-# updates repositories
+# update repositories
+sudo apt update
 
+# install dependancies
 for package in ${dependancies[@]}
     do
 
@@ -32,6 +34,8 @@ done
 # clones the rust project from github
 if [ -z $(find xf-tweaks)  ]
     then
-    echo "cloning code from github..."
-    # git clone https://github.com/alexou2/xf-tweaks.git
+    echo "cloning code from github in 2 sec..."
+    # leaves time to cancel for debugging
+    sleep 2
+    git clone https://github.com/alexou2/xf-tweaks.git
 fi
